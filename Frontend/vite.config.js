7b3bebc47f8fb7ca94ce34e16df952668598e7c4
Rutvik <rutvikraut1@gmail.com>
2023-12-20 22:22:12 +0530

    updated backend url

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:5500' //whenever '/api' is there this will append automatically
+      '/api': 'https://ems-backend-ksng.onrender.com' //whenever '/api' is there this will append automatically
     }
   },
   plugins: [react()],
